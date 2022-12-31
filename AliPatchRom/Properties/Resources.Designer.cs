e393eeecb139af1dfb2d2cdb39e617c627acbc1c
Phil.Xing <phil.xing@adlinktech.com>
2022-12-31 23:34:22 +0800

    Rename the solution and the project.

--- AliPatchRom/Properties/Resources.Designer.cs
+++ AliPatchRom/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AliPatchRom.Properties {
+namespace AliPatch.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AliPatchRom.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AliPatchRom.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AliPatch.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- AliPatchRom/Properties/Settings.Designer.cs
+++ AliPatchRom/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AliPatchRom.Properties {
+namespace AliPatch.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
